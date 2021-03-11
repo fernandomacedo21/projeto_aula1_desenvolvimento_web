e24264941e417b87826732ae9ab2a5a219f802e7
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-03-11 19:05:07 -0300

    Desenvolvendo site em HTML,CSS,JAVASCRIPT e FLASK

--- scripts/verifica.js
+++ scripts/verifica.js
@@ -2,14 +2,46 @@
 const botao_enviar = document.getElementById("bt-enviar");
 const botao_limpar = document.getElementById("bt-limpar");
 const nome = document.getElementById("txt-nome");
+const email = document.getElementById("txt-email");
 const mensagem = document.getElementById("txt-mensagem");
 const p_erros = document.getElementById("erros");
 
 botao_enviar.addEventListener("click", function(event){
+    let erros = "";
     if(nome.value == ""){
         // Cometario: bloc de codigo que excuta caso o if seja verdadeiro
         //alert("O nome não pode estar em branco");
-        p_erros.innerHTML = "O nome não pode estar em branco!";
+        erros = erros + "O nome não pode estar em branco!<br>";
         event.preventDefault();
     }
+    if(email.value == ""){
+        erros = erros + "O e-mail não pode estar vazia!<br>";
+        event.preventDefault();
+    }
+    if(mensagem.value == ""){
+        erros = erros + "A mensagem não pode estar vazia!";
+        event.preventDefault();
+    }
+    p_erros.innerHTML = erros;
 });
+
+/*
+//Posso escrever os eventos de click do botão limpar e mudança do campo nome como abaixo
+botao_limpar.addEventListener("click", function(event){
+    p_erros.innerText = "";
+});
+
+nome.addEventListener("change", function(event){
+    nome.value = nome.value.toUpperCase();
+});
+*/
+
+//ou posso escrever od dois eventos acima da seguinte forma:
+
+botao_limpar.onclick = function(event){
+    p_erros.innerText = "";
+}
+
+nome.onchange = function(event){
+    nome.value = nome.value.toUpperCase();
+}
